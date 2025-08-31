af74f6d6e2081183edc2b5d9a7144d869fd97b1e
httpsAKayush <ak4895931@gmail.com>
2025-08-31 16:11:33 +0530

    py script

---
+++ test.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import os
+
+data = tf.keras.utils.image_dataset_from_directory(
+    'test',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(256, 256),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+    )
+print(labels)
